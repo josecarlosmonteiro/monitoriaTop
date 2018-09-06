8a8b26fa9a3f864fc57f4e2867d66b254f01bcc6
jackson29 <jacksonssantos@bol.com.br>
2018-09-06 13:50:29 -0300

    telas iniciais

---
+++ php/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="../css/login.css">
+	<title>Login - Relatório Digital</title>
+</head>
+<body>
+	<fieldset>
+		<h3>Relatório digital</h3>
+		<h4>Login</h4>
+		<br>
+		<form action="#" method="POST">
+			<input type="text" placeholder="Matrícula" required><br>
+			<input type="password" placeholder="Senha" required><br>
+			<input type="submit" value="Login" id="sub">
+		</form>
+	</fieldset>
+</body>
+</html>
