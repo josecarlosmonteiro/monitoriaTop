5008912f6390a134b043c79606a0b554f727bfd0
josecarlosmonteiro <1monteirocarlos@gmail.com>
2018-09-08 02:26:51 -0300

    Atualização de responsividade

--- php/cadastro.php
+++ php/cadastro.php
@@ -8,6 +8,7 @@
 </head>
 <body>
 	<fieldset>
+		<h3>Cadastro</h3>
 		<form action="#" method="POST">
 			<input type="text" name="nome" placeholder="Nome" required>
 			<input type="text" name="nome" placeholder="Matrícula" required>
@@ -17,11 +18,15 @@
 					<option>IPI</option>
 					<option>LOG</option>
 				</select>
+			</label><br>
+			<label>
 				Período: <select name="periodo" required>
 					<option></option>
 					<option>2</option>
 					<option>3</option>
 				</select>
+			</label><br>
+			<label>
 				Cadeira: <select name="cadeira" required>
 					<option></option>
 					<option>###</option>
