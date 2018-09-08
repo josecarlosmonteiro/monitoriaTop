5b4d9ccdc1da988c6c302b65e80faf4efca0f568
eduardobispof <eduardobispof@github.com>
2018-09-08 07:43:38 -0300

    codigo do banco com modificações nas tabelas dos monitores e adidionando tabela para professores

--- sql/banco.sql
+++ sql/banco.sql
@@ -7,29 +7,38 @@ CREATE TABLE monitor(
 	matricula VARCHAR(50) NOT NULL,
 	nome VARCHAR(255) NOT NULL,
 	curso VARCHAR(50) NOT NULL,
-
-	-- tipo da coluna periodo ainda não definida
 	periodo VARCHAR(3) NOT NULL,
-	PRIMARY KEY(id_registro)
+	PRIMARY KEY(id_monitor)
 );
 
 -- criar tabela registro: irá armazenar os registros das aulas/CH semanal das bolsas
 CREATE TABLE registro(
 	id_registro INT NOT NULL AUTO_INCREMENT,
-    matricula VARCHAR,
+    id_monitor VARCHAR(50),
     curso_monitoria VARCHAR(50) NOT NULL,
+    periodo_monitoria VARCHAR(50) NOT NULL,
     data_monitoria DATE NOT NULL,
     hora_inicio TIME NOT NULL,
     hora_termino TIME NOT NULL,
+    comentario VARCHAR(500),
     data_registro VARCHAR NOT NULL,
-    FOREIGN KEY matricula REFERENCES monitor(matricula)
+    FOREIGN KEY id_monitor REFERENCES monitor(id_monitor)
 );
 
 -- criar tabela para senhas
 CREATE TABLE senha(
 	id_senha INT NOT NULL,
-	matricula VARCHAR,
+	id_monitor VARCHAR,
 	senha VARCHAR(255) NOT NULL,
 	PRIMARY KEY(id_senha),
-	FOREIGN KEY matricula REFERENCES monitor(matricula)	
+	FOREIGN KEY id_monitor REFERENCES monitor(id_monitor)	
 );
+
+
+------------------------------PROFESSOR------------------------------
+
+CREATE TABLE cadastro(
+	id_cadastro INT NOT NULL AUTO_INCREMENT,
+	matricula VARCHAR(50) NOT NULL,
+	PRIMARY KEY (id_cadastro0)
+)
