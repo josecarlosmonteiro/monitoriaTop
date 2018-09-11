d57a41663b1ecb455bd938b45d0d8d0483485532
eduardobispof <eduardobispof@gmail.com>
2018-09-11 12:35:40 -0300

    FINALMENTE ESSA DESGRAÇA TA SEM ERRO

--- sql/banco.sql
+++ sql/banco.sql
@@ -14,25 +14,25 @@ CREATE TABLE monitor(
 -- criar tabela registro: irá armazenar os registros das aulas/CH semanal das bolsas
 CREATE TABLE registro(
 	id_registro INT NOT NULL AUTO_INCREMENT,
-    id_monitor VARCHAR(50),
+    id_monitor_rg VARCHAR(50),
     curso_monitoria VARCHAR(50) NOT NULL,
     periodo_monitoria VARCHAR(50) NOT NULL,
     data_monitoria DATE NOT NULL,
     hora_inicio TIME NOT NULL,
     hora_termino TIME NOT NULL,
     comentario VARCHAR(500),
-    data_registro VARCHAR NOT NULL,
+    data_registro DATE NOT NULL,
     PRIMARY KEY (id_registro),
-    FOREIGN KEY id_monitor REFERENCES monitor(id_monitor)
+    FOREIGN KEY (id_monitor_rg) REFERENCES monitor(id_monitor)
 );
 
 -- criar tabela para senhas
 CREATE TABLE senha(
 	id_senha INT NOT NULL,
-	id_monitor VARCHAR,
+	id_monitor_s VARCHAR,
 	senha VARCHAR(255) NOT NULL,
 	PRIMARY KEY(id_senha),
-	FOREIGN KEY id_monitor REFERENCES monitor(id_monitor)	
+	FOREIGN KEY (id_monitor_s) REFERENCES monitor(id_monitor)	
 );
 
 
