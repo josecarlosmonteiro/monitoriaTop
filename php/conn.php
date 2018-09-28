71bbeb5336f7cd46fb95726306fb4e4dc535b2fd
eduardobispof <eduardobispof@gmail.com>
2018-09-28 16:14:16 -0300

    inicio JS

--- php/conn.php
+++ php/conn.php
@@ -1,14 +1,13 @@
 <?php
 header('location: home.php');
-<?php
 	
-	$dbname = "id7135993_monitoria";
-	$usuario="id7135993_monitoriatop";
-	$senha = "monitoriatop";
-	try {
-	  	$conn = new PDO("mysql:host=localhost;dbname=$dbname", $usuario, $senha);
-	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	} catch(PDOException $e) {
-	    echo 'ERROR: ' . $e->getMessage();
-	}
+$dbname = "id7135993_monitoria";
+$usuario="id7135993_monitoriatop";
+$senha = "monitoriatop";
+try {
+	$conn = new PDO("mysql:host=localhost;dbname=$dbname", $usuario, $senha);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+	echo 'ERROR: ' . $e->getMessage();
+}
 ?>
