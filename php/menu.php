9531ab554317afe8ca5bd3b9114414bb2dc8ddd3
josecarlosmonteiro <1monteirocarlos@gmail.com>
2018-10-19 15:59:40 -0300

    início de tela de horários

--- php/menu.php
+++ php/menu.php
@@ -1,7 +1,4 @@
-<?php 
-include 'conn.php';
-session_start();
- ?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -14,16 +11,14 @@ session_start();
 		<ul>
 			<input type="checkbox" id="btnMenu" style="display: none;">
 			<label for="btnMenu" id="btn">&#9776;</label>
-			<a id="user" href="#"><?=$_SESSION['user']?>- CRS-1</a>
+			<a id="user" href="#"><?=$_SESSION['user']?> - CRS-1</a>
 			<li><a href="home.php">Início</a></li>
 
 			<?php 
 			if ($_SESSION['tipo'] == "monitor") : ?>
 				<li><a href="#">Documentos</a></li>
-				<li><a href="#">Atividades</a></li>
 			<?php endif ?>
-
-			<li><a href="#">Avisos</a></li>
+				<li><a href="#">Atividades</a></li>
 			<li><a href="logout.php">Sair</a></li>
 		</ul>
 	</nav>
