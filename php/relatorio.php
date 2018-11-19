cf40ee3dee6a861e9b95d4abfda0804503e5d7ba
eduardobispof <eduardobispof@gmail.com>
2018-11-19 12:57:08 -0300

    gerando pdf, só falta estilizar

--- php/relatorio.php
+++ php/relatorio.php
@@ -1,15 +1,10 @@
 <?php 
 include 'conn.php';
 session_start();
-
-if (!isset($_SESSION['matricula'])) {
-	header('location: ../index.php');
-}
-
+//çoshfpokjh
 $dataP = filter_input_array(INPUT_POST);
 $matricula = addslashes($_SESSION['matricula']);
 
-/*
 $query = $conn->prepare("SELECT m.titulo_atividade, m.data_monitoria, m.inicio_monitoria, m.termino_monitoria FROM monitoria m INNER JOIN aluno a ON m.matricula_monitor = a.matricula WHERE a.matricula = ? AND a.tipo = 'monitor' AND m.data_monitoria > ? AND m.data_monitoria < ?");
 $query->bindParam(1, $matricula);
 $query->bindParam(2, $dataP['inicio']);
@@ -17,25 +12,27 @@ $query->bindParam(3, $dataP['termino']);
 $query->execute();
 $data = $query->fetchALL(PDO::FETCH_ASSOC);
 $html = "";
-var_dump($data);
+
 foreach ($data as $lista) {
 	$html .= $lista['titulo_atividade'];
 	$html .= $lista['data_monitoria'];
 	$html .= $lista['inicio_monitoria'];
 	$html .= $lista['termino_monitoria'];
 }
-*/
-require_once '../pdf/vendor/autoload.php';
 
-//referencia o namespase do DOMPDF
+// header('location: pdf.php');
+// echo $html;
+
+
+
 use Dompdf\Dompdf;
 
 //instancia o DOMPDF
 $dompdf = new Dompdf();
 
 // $html = file_get_contents('relatorio.php')
-include('relatorio.php');
-$html = getData();
+// include('relatorio.php');
+// $html = getData();
 
 //inserindo o HTML no PDF;
 $dompdf->loadHtml ($html);
@@ -48,4 +45,5 @@ $dompdf->render();
 
 //enviando o PDF pro navegador
 $dompdf->stream('relatorio.pdf', array('Attachment' => false));
+
  ?>
