cba8d4d5b14169d4baeccf2bfb40c45b8e55fb65
Murielson <jhonsnoow32@gmail.com>
2018-11-23 00:57:05 -0300

    Retoques Finais

--- php/userPag.php
+++ php/userPag.php
@@ -4,9 +4,30 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Perfil</title>
+	<title>Profile</title>
+	<link rel="stylesheet" href="../css/user.css">
 </head>
 <body>
-	
+	<div class="content">
+		<div class="content-header">
+			<div class="icons">
+				<i class="fab fa-facebook"></i>
+				<i class="fab fa-instagram"></i>
+				<i class="fab fa-twitter-square"></i>
+				<i class="fas fa-envelope-square"></i>
+			</div>
+			<div class="content-image"></div>
+			<button><i class="fas fa-user-edit"></i>  Editar Perfil</button>
+		</div>
+		<div class="content-body">
+			<?php if ($_SESSION['tipo'] == 'monitor'):?>
+			<h2><?php echo $_SESSION['user']." ".$_SESSION['sobrenome']." "; ?><span style="color: red;"><?= $_SESSION['periodo']; ?>° Periodo - <?= $_SESSION['curso'] ?></span></h2><br>
+			<h3>Monitor de <span style="color: red;"><?= $_SESSION['cadeira_monitor']; ?></span></h3><br>
+			<?php else: ?>
+			<h2><?php echo $_SESSION['user']." ".$_SESSION['sobrenome']." "; ?><span style="color: red;"><?= $_SESSION['periodo']; ?>° Periodo - <?= $_SESSION['curso'] ?></span></h2><br>
+			<h3>Aluno</h3><br>
+			<?php endif; ?>
+		</div>
+	</div>
 </body>
 </html>
